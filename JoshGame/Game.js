e0fc01d6c5909ae95e34d4c2a1eff158b96c05b8
Caleb <caleblgentry@gmail.com>
2016-06-08 16:44:43 -0400

    Update Game.js

--- JoshGame/Game.js
+++ JoshGame/Game.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {preload: preload, create: create, update: update});
 
 function preload() {
-   game.add.image('chicken', '/assets/chicken.png');
+   game.load.image('chicken', '/assets/chicken.png');
 
 }
 var chicken;
