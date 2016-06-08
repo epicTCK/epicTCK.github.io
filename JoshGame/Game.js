e11f444ed29cbd94d058c5082148a8fc6eb9c91b
Caleb <caleblgentry@gmail.com>
2016-06-08 17:00:40 -0400

    Update Game.js

--- JoshGame/Game.js
+++ JoshGame/Game.js
@@ -9,6 +9,7 @@ function create() {
    chicken = Game.add.sprite(0, 0, 'chicken');
    Game.physics.startSystem(Phaser.Physics.ARCADE);
    Game.physics.enable(chicken, Phaser.Physics.ARCADE);
+   Game.physics.arcade.gravity.y = 200;
 
 }
 function update(){
