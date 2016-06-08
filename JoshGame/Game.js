50c7ce018a3438572308d1ac5ff490767e43f54b
Caleb <caleblgentry@gmail.com>
2016-06-08 16:58:36 -0400

    Update Game.js

--- JoshGame/Game.js
+++ JoshGame/Game.js
@@ -1,12 +1,12 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {preload: preload, create: create, update: update});
+var Game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {preload: preload, create: create, update: update});
 
 function preload() {
-   game.load.image('chicken', 'assets/Chicken.jpeg');
+   Game.load.image('chicken', 'assets/Chicken.jpeg');
 
 }
 var chicken;
 function create() {
-   chicken = game.add.sprite(0, 0, 'chicken');
+   chicken = Game.add.sprite(0, 0, 'chicken');
    Game.physics.startSystem(Phaser.Physics.ARCADE);
    Game.physics.enable(chicken, Phaser.Physics.ARCADE);
 
