14e2b8a339241b4410c1bb7282a03fd24b1ccda0
Caleb <caleblgentry@gmail.com>
2016-06-08 16:20:07 -0400

    Add files via upload

---
+++ JoshGame/Game.js
@@ -0,0 +1,10 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example’, {preload: preload, create: create, update: update});
+
+function preload() {
+   game.add.image('chicken', ‘/assets/chicken.png’);
+
+}
+var chicken;
+function create() {
+   chicken = game.add.sprite(0, 0, 'chicken');
+}
