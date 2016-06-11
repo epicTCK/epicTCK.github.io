bd897794c28a282ffe5c88235816e9fcbeb10aa2
Caleb <caleblgentry@gmail.com>
2016-06-11 07:57:12 -0400

    Input

--- JoshGame/Game.js
+++ JoshGame/Game.js
@@ -5,13 +5,19 @@ function preload() {
 
 }
 var chicken;
+var cursors;
 function create() {
    chicken = Game.add.sprite(0, 0, 'chicken');
    Game.physics.startSystem(Phaser.Physics.ARCADE);
    Game.physics.enable(chicken, Phaser.Physics.ARCADE);
    Game.physics.arcade.gravity.y = 200;
    chicken.body.collideWorldBounds = true;
+   cursors = Game.input.keyboard.createCursorKeys();
 }
 function update(){
-   console.log('hi');
+   cursors.body.velocity.x = 0;
+   if(cursors.right.isDown)
+     chicken.body.velocity.x = 50;
+   if(cursors.left.isDown)
+     chicken.body.velocity.x = -50;
 }
